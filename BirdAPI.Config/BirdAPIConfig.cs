ab7b4abb268db275d238745516706bc5f4f9b975
DavinoLiessens <liessda@cronos.be>
2022-06-09 10:19:11 +0200

    Basic setup with Domain

---
+++ BirdAPI.Config/BirdAPIConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdAPI.Config
+{
+    public class BirdAPIConfig
+    {
+        public string ConnectionString { get; set; }
+        public string Authority { get; set; }
+    }
+}
