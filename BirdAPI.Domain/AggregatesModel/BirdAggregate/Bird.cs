fb73cec5446f5d20f720859e173f22457bed5126
DavinoLiessens <liessda@cronos.be>
2022-06-09 18:03:31 +0200

    Added responsemodels and automapper for bird

--- BirdAPI.Domain/AggregatesModel/BirdAggregate/Bird.cs
+++ BirdAPI.Domain/AggregatesModel/BirdAggregate/Bird.cs
@@ -21,5 +21,40 @@ namespace BirdAPI.Domain.AggregatesModel.BirdAggregate
         public int OwnerId { get; set; }
         public string Description { get; set; }
         public bool Dead { get; set; }
+
+        protected Bird()
+        {
+
+        }
+
+        public Bird(string ringNumber, string sex, string kindOfBird, string ageOfBirth, string color, int cageNumber, string creator, string description, bool dead = false)
+        {
+            RingNumber = ringNumber;
+            Sex = sex;
+            KindOfBird = kindOfBird;
+            AgeOfBirth = ageOfBirth;
+            Color = color;
+            CageNumber = cageNumber;
+            Creator = creator;
+            Description = description;
+            Dead = dead;
+        }
+
+        public Bird BelongsToOwner(Owner owner)
+        {
+            Owner = owner;
+            OwnerId = owner.Id;
+
+            return this;
+        }
+
+        public Bird UpdateBird(int? cageNumber, string? description, bool? dead)
+        {
+            CageNumber = cageNumber.Value;
+            Description = description;
+            Dead = dead.Value;
+
+            return this;
+        }
     }
 }
