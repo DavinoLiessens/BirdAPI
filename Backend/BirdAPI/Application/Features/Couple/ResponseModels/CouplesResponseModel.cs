62b7220c24f4241f32e2885a750f848c1fed84d4
Davino_Liessens <liessda@cronos.be>
2023-11-01 21:35:03 +0100

    Update missing prop response model

--- Backend/BirdAPI/Application/Features/Couple/ResponseModels/CouplesResponseModel.cs
+++ Backend/BirdAPI/Application/Features/Couple/ResponseModels/CouplesResponseModel.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int CageNumber { get; set; }
+        public string CageNumber { get; set; }
         public DateTime StartedAt { get; set; }
         public string MotherRingNumber { get; set; }
         public string FatherRingNumber { get; set; }
