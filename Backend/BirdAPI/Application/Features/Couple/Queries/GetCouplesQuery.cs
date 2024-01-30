191029b74ecd0362d60f03cc20c5669b05f40549
Davino_Liessens <liessda@cronos.be>
2024-01-31 00:11:25 +0100

    Added backend search for Couples

--- Backend/BirdAPI/Application/Features/Couple/Queries/GetCouplesQuery.cs
+++ Backend/BirdAPI/Application/Features/Couple/Queries/GetCouplesQuery.cs
@@ -8,11 +8,13 @@ namespace BirdAPI.Application.Features.Couple.Queries
     {
         public int? Page { get; set; }
         public int? PageSize { get; set; }
+        public string? SearchValue { get; set; }
 
-        public GetCouplesQuery(int? page, int? pageSize)
+        public GetCouplesQuery(int? page, int? pageSize, string? searchValue)
         {
             Page = page;
             PageSize = pageSize;
+            SearchValue = searchValue;
         }
     }
 }
