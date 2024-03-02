7f137b251537ab6e0e18d2fb0d42146166749926
Davino_Liessens <liessda@cronos.be>
2024-03-02 12:01:08 +0100

    Added test for publish profile

--- Backend/BirdAPI/Program.cs
+++ Backend/BirdAPI/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<BirdAPIContext>(options =>
 {
-    options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=db-bird;Trusted_Connection=True;", sqloptions =>
+    options.UseSqlServer("Server=tcp:bird-platform.database.windows.net,1433;Initial Catalog=db-bird-platform;Persist Security Info=False;User ID=bird-admin;Password=Zeppos123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;", sqloptions =>
     {
         sqloptions.MigrationsAssembly(typeof(Program).Assembly.GetName().Name);
         sqloptions.EnableRetryOnFailure(maxRetryCount: 10, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
@@ -24,17 +24,17 @@ builder.Services.AddDbContext<BirdAPIContext>(options =>
 });
 builder.Services.AddHostedService<InitializeDatabaseService>();
 builder.Services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp"; });
-
+builder.Services.AddApplicationInsightsTelemetry();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     // uncomment this if you want to access the Swagger UI view !!!
     //app.UseSwaggerUI();
-}
+//}
 
 app.UseCors(builder =>
     builder.AllowAnyOrigin()
@@ -52,23 +52,23 @@ app.UseSpa(spa => { spa.Options.SourcePath = "ClientApp"; });
 
 app.MapControllers();
 
-app.UseEndpoints(endpoints =>
-{
-    // Default Fallback Route: Redirect to /ClientApp/index.html for any other unmatched route
-    endpoints.MapFallbackToFile("/ClientApp/index.html");
+//app.UseEndpoints(endpoints =>
+//{
+//    // Default Fallback Route: Redirect to /ClientApp/index.html for any other unmatched route
+//    endpoints.MapFallbackToFile("/ClientApp/index.html");
 
-    // Custom Endpoint: Redirect to /ClientApp/index.html
-    endpoints.MapGet("/ClientApp", async context =>
-    {
-        context.Response.Redirect("/ClientApp/index.html");
-    });
+//    // Custom Endpoint: Redirect to /ClientApp/index.html
+//    endpoints.MapGet("/ClientApp", async context =>
+//    {
+//        context.Response.Redirect("/ClientApp/index.html");
+//    });
 
-    // Custom Endpoint: Redirect to /swagger/index.html
-    endpoints.MapGet("/swagger", async context =>
-    {
-        context.Response.Redirect("/swagger/index.html");
-    });
+//    // Custom Endpoint: Redirect to /swagger/index.html
+//    endpoints.MapGet("/swagger", async context =>
+//    {
+//        context.Response.Redirect("/swagger/index.html");
+//    });
 
-});
+//});
 
 app.Run();
